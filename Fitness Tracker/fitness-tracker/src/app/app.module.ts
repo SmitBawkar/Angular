ab65ec306188bba90cf02a66cf4a3dbf135c3c37
smit bawkar <smit399@gmail.com>
2019-07-15 10:20:52 +0530

    fitness traker app initial commit

---
+++ Fitness Tracker/fitness-tracker/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MaterialComponent} from './material.module'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Fitness Tracker/fitness-tracker/src/app/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule, MatIconModule} from '@angular/material';
+
+@NgModule({
+  imports : [],
+  exports : [MatButtonModule,MatIconModule]
+})
+export class MaterialComponent{}
